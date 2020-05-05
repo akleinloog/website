8033c2fba2713eeaca54b12c370de20ca9ea7488
Arnoud Kleinloog <akleinloog@gmail.com>
2020-05-05 21:44:16 +0200

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,29 +4,26 @@ set -e
 
 echo "Cleaning Publish Folder"
 
-rm -r /path/to/dir/*
+rm -r ./public/*
 
 echo "Running Hugo Build"
 
-# Build the project.
-hugo -t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t academic
 
-# Go To Public folder
 cd public
 
 echo "Adding changes to git"
 
-# Add changes to git.
-#git add .
+git add .
 
-# Commit changes.
-#msg="rebuilding site $(date)"
-#if [ -n "$*" ]; then
-#	msg="$*"
-#fi
-#git commit -m "$msg"
+msg="regenerating site content $(date)"
+
+# git commit -m "$msg"
 
 echo "Pushing to master"
 
-# Push source and build repos.
-#git push origin master
+# git push origin master
+
+cd ..
+
+echo "Deployment Finished"
